09da6228fc0f69e7d6f53603d75088a2bd8c1cdb
ironfinger <tomhoughton888@gmail.com>
2017-06-03 19:48:14 +0100

    Changed styling and addedd buttons

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 	$('.parallax').parallax();
 });
+
+var options = [
+	{selector: '#row1', offset: 400, callback: function(el) {
+		Materialize.showStaggeredList($(el));
+	}},
+	{selector: '#row2', offset: 400, callback: function(el){ 
+		Materialize.showStaggeredList($(el));
+	}}
+];
+Materialize.scrollFire(optons);
